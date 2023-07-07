c5d9841766f886fb936b352ddf5cab78c3b94677
Bach Nguyen <bachnx2303@gmail.com>
2023-07-07 16:44:45 +0700

    optimize: Installation selector

--- enviroment-setup.sh
+++ enviroment-setup.sh
@@ -8,10 +8,9 @@ SUCCESS='\033[1;32m'
 BLACK='\033[1;30m'
 WHITE='\033[1;37m'
 
-
-executeInstall() {
-
-    ## Add repo
+apacheInstallation() {
+    echo -e $SUCCESS "Apache 2.4 installation selected. Please wait." $RESET_COLOR
+    # Add repo
     echo -e $WHITE"***\nCheck HTTPD"$RESET_COLOR
     check_httpd=$(yum list installed | grep httpd)
     if [[ ! $check_httpd ]];then
@@ -25,7 +24,38 @@ executeInstall() {
         http_version=$(httpd -v | awk '/Server version: / {print $3;}')
         echo -e $WARNING"Installed version ${http_version}"$RESET_COLOR
     fi
+}
 
+phpInstallation() {
+    echo -e $SUCCESS "PHP installation selected. Please wait." $RESET_COLOR
+    echo -e $WHITE"***\nCheck PHP"$RESET_COLOR
+    check_php=$(yum list installed | grep php)
+    if [[ ! $check_php ]];then
+        sudo yum install epel-release yum-utils
+        read -p $WHITE"What version php want install? (70,71,72,73,74,80) "$RESET_COLOR phpVersion
+        echo -e $WHITE"PHP version $phpVersion will be install in this server"$RESET_COLOR;
+        echo "#################################################################";
+        echo "Start install PHP ${phpVersion}"
+
+        yum-config-manager --enable remi-php"${phpVersion}"
+        yum -y install php
+        yum -y install unzip php-opcache php-cli php-xml php-pecl-zip php-mbstring php-pdo php-pdo_mysql php-gd php-mcrypt php-redis php-bcmath php-soap php-devel php-dom
+        yum groupinstall " Development Tools"  -y
+        yum install ImageMagick ImageMagick-devel -y
+        yum install php-pear -y
+        yum install php-devel -y
+        pecl install Imagick
+
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+    else       
+        php_version=$(php -v | awk '/PHP / {print $2;}' | sed -e 's/(c)//g')
+        echo -e $WARNING"Installed version ${php_version}"$RESET_COLOR
+    fi     
+}
+
+mysqlInstallation() {
+    echo -e $SUCCESS "MySQL installation selected. Please wait." $RESET_COLOR
     echo -e $WHITE"***\nCheck MySQL"$RESET_COLOR
     check_mysql=$(yum list installed | grep mysql)
     if [[ ! $check_mysql ]]; then
@@ -43,27 +73,11 @@ executeInstall() {
         mysql_version=$(mysql -V | awk '/Distrib/ {print $5;}' | sed -e 's/,//g')
         echo -e $WARNING"Installed version ${mysql_version}"$RESET_COLOR
     fi
+}
 
-    echo -e $WHITE"***\nCheck PHP"$RESET_COLOR
-    check_php=$(yum list installed | grep php)
-    if [[ ! $check_php ]];then
-        sudo yum install epel-release yum-utils
-        read -p $WHITE"What version php want install? (70,71,72,73,74,80) "$RESET_COLOR phpVersion
-        echo -e "\033[0;34mPHP version $phpVersion will be install in this server\033[\r";
-        echo "#################################################################";
-        echo "Start install PHP ${phpVersion}"
+javaInstallation() {
+    echo -e $SUCCESS "Java installation selected. Please wait." $RESET_COLOR
 
-        yum-config-manager --enable remi-php"${phpVersion}"
-        yum -y install php
-        yum -y install unzip php-opcache php-cli php-xml php-pecl-zip php-mbstring php-pdo php-pdo_mysql php-gd php-mcrypt php-redis php-bcmath php-soap php-devel php-dom
-
-        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-        php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-    else       
-        php_version=$(php -v | awk '/PHP / {print $2;}' | sed -e 's/(c)//g')
-        echo -e $WARNING"Installed version ${php_version}"$RESET_COLOR
-    fi      
-    
     echo -e $WHITE"***\nCheck JAVA"$RESET_COLOR
     check_java=$(yum list installed | grep java)
     if [[ ! $check_java ]];then
@@ -72,7 +86,10 @@ executeInstall() {
         java_version=$(java -version 2>&1)
         echo -e $WARNING"${java_version}"$RESET_COLOR
     fi
+}
 
+elasticsearchInstallation() {
+    echo -e $SUCCESS "Elasticsearch installation selected. Please wait." $RESET_COLOR
     echo -e $WHITE"***\nCheck Elastichsearh"$RESET_COLOR
     check_els=$(yum list installed | grep elasticsearch)
     if [[ ! $check_els ]];then
@@ -95,7 +112,10 @@ executeInstall() {
         els_version=$(curl  -s http://localhost:9200 | awk '/number/ {print $3;}' | sed -e 's/"//g' | sed -e 's/,//')
         echo -e $WARNING"Installed version ${els_version}"$RESET_COLOR
     fi
+}
 
+nodeJsInstallation() {
+    echo -e $SUCCESS "NodeJS installation selected. Please wait." $RESET_COLOR
     echo -e $WHITE"***\nCheck NODEJS"$RESET_COLOR
     check_nodejs=$(yum list installed | grep node)
     if [[ ! $check_nodejs ]];then
@@ -109,7 +129,10 @@ executeInstall() {
         node_version=$(node --version | sed -e 's/v//g')
         echo -e $WARNING"Installed version ${node_version}"$RESET_COLOR
     fi
+}
 
+gitInstallation() {
+    echo -e $SUCCESS "GIT installation selected. Please wait." $RESET_COLOR
     echo -e $WHITE"***\nCheck GIT"$RESET_COLOR
     check_git=$(yum list installed | grep git)
     if [[ ! $check_git ]];then
@@ -120,6 +143,10 @@ executeInstall() {
         echo -e $WARNING"Installed version ${git_version}"$RESET_COLOR
     fi
 
+}
+
+redisInstallation() {
+    echo -e $SUCCESS "REDIS installation selected. Please wait." $RESET_COLOR
     echo -e $WHITE"***\nCheck REDIS"$RESET_COLOR
     check_redis=$(yum list installed | grep redis)
     if [[ ! $check_redis ]];then
@@ -132,7 +159,10 @@ executeInstall() {
         redis_version=$(redis-server --version | awk '/v=/ {print $3;}' | sed -e 's/v=//g')
         echo -e $WARNING"Installed version ${redis_version}"$RESET_COLOR
     fi
+}
 
+mavenInstallation() {
+    echo -e $SUCCESS "MAVEN installation selected. Please wait." $RESET_COLOR
     echo -e $WHITE"***\nCheck Maven"$RESET_COLOR
     check_maven=$(yum list installed | grep mvn)
     if [[ ! $check_maven ]];then
@@ -141,16 +171,49 @@ executeInstall() {
     else 
         maven_version=$(mvn --version | awk '/Apache/ {print $3;}')
         echo -e $WARNING"Installed version ${maven_version}"$RESET_COLOR
-    fi      
-
+    fi   
 }
-echo -e $WARNING"Do you want to new install? (yes/no) " $RESET_COLOR
-read -p "" yn
 
-case $yn in
-        yes ) executeInstall ;;
-        no ) echo bye bye...;
-                exit;;
-        * ) echo invalid response;
-                exit 1;;
-esac
+fullInstallation() {
+    echo -e $SUCCESS "Full installation webserver selected. Please wait." $RESET_COLOR
+    apacheInstallation ;
+    gitInstallation ;
+    nodeJsInstallation ;
+    phpInstallation ;
+    redisInstallation ;
+    mysqlInstallation ;
+}
+
+echo -e $DANGER"Select one options \n" $RESET_COLOR
+
+
+select opt in "Full Installation" "Apache2.4" "PHP" "MySQL" "Elasticsearch" "NodeJS" "GIT" "REDIS" "MAVEN" "JAVA" "Quit"
+do
+    case $opt in
+        "Full Installation")
+            fullInstallation ;;
+        "Apache2.4")
+            apacheInstallation ;;
+        "PHP")
+            phpInstallation ;;
+        "MySQL")
+            mysqlInstallation ;;
+        "Elasticsearch")
+           elasticsearchInstallation ;;
+        "NodeJS")
+           nodeJsInstallation ;;
+        "GIT")
+           gitInstallation ;;
+        "REDIS")
+           redisInstallation ;;
+        "MAVEN")
+           mavenInstallation ;;
+        "JAVA")
+           javaInstallation ;;
+        "Quit")
+           echo -e $BLACK"We're done. bye bye..." $RESET_COLOR
+           break;;
+        *)
+           echo "Ooops";;
+    esac
+done
