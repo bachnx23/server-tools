bb1b7a21ab7a373c1f99f62c78e2a272d59efdee
bachnx23 <bachnx2303@gmail.com>
2024-09-25 11:43:46 +0700

    Update file_browser.php

--- file_browser.php
+++ file_browser.php
@@ -1,7 +1,7 @@
 <?php
 // Thông tin xác thực
 $valid_username = 'megaads';
-$valid_password = 'Megaads@12345678aa';
+$valid_password = 'xxxxxxx';
 
 // Hàm để yêu cầu xác thực
 function authenticate() {
